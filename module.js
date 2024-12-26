909281644c400397365008552b30cacb64fbd567
PawKow2K <paw.kow.2k@gmail.com>
2024-12-26 15:44:03 +0100

    work

--- module.js
+++ module.js
@@ -1,5 +1,19 @@
+const { DataTypes } = require("sequelize");
+const { ModelDef } = require("database.js");
+
 module.exports = {
-  commands: [
-    require('./utils/bingus.js'),
-  ]
-}
+  commands: [require("./utils/bingus.js"), require("./utils/timereminder.js")],
+  models: [
+    ModelDef("NotificationHour", {
+      userId: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        primaryKey: true,
+      },
+      hour: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+    }),
+  ],
+};
---
+++ utils/timereminder.js
@@ -0,0 +1,32 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ChatInputCommandInteraction,
+} = require("discord.js");
+const { db } = require("database.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("setremindertime")
+    .setDescription("Set time for your personal vocal reminder")
+    .addNumberOption((option) => {
+      option
+        .setName("hour")
+        .setDescription("Hour of the reminder")
+        .setRequired();
+    })
+    .addNumberOption((option) => {
+      option
+        .setName("minute")
+        .setDescription("Minute of the reminder (optional)");
+    }),
+
+  async execute(interaction) {
+    var hour = interaction.options.getNumberOption("hour");
+    var minute = interaction.options.getNumberOption("minute");
+    await interaction.reply({
+      content: `Set the notification time to ${hour}`,
+      ephemeral: true,
+    });
+  },
+};
